18b089fa312afed84e0afb312d9ca2b9b41fb2af
Zane Riley <zaneriley@gmail.com>
2017-01-30 19:30:13 -0800

    Add s3 deploy

--- tools/publish.js
+++ tools/publish.js
@@ -8,18 +8,22 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-const path = require('path');
-const firebase = require('firebase-tools');
-const build = require('./build');
 const task = require('./task');
-const config = require('./config');
+const s3 = require('s3');
+const s3Options = require('../../personal-site-s3-creds.json');
 
-// Build and deploy the app to Firebase
-module.exports = task('deploy', () => Promise.resolve()
-  .then(() => build())
-  .then(() => firebase.login({ nonInteractive: false }))
-  .then(() => firebase.deploy({
-    project: config.project,
-    cwd: path.resolve(__dirname, '../'),
-  }))
-  .then(() => { setTimeout(() => process.exit()); }));
+// deploy the app to Amazon s3
+global.DEBUG = process.argv.includes('--debug') || false;
+
+module.exports = task('publish', () => new Promise((resolve, reject) => {
+  const client = s3.createClient({
+    s3Options: s3Options,
+  });
+  const uploader = client.uploadDir({
+    localDir: 'public',
+    deleteRemoved: true,
+    s3Params: { Bucket: 'zaneriley' }, 
+  });
+  uploader.on('error', reject);
+  uploader.on('end', resolve);
+}));
