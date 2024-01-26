4a98e481348e5496654e7267345878ac3f527689
zaneriley <zaneriley@gmail.com>
2024-01-26 19:16:41 +0900

    don't locale redirect static assets

--- lib/portfolio_web/plugs/set_locale.ex
+++ lib/portfolio_web/plugs/set_locale.ex
@@ -2,45 +2,60 @@ defmodule PortfolioWeb.Plugs.SetLocale do
   import Phoenix.Controller, only: [redirect: 2]
   import Plug.Conn
   import PortfolioWeb.Gettext
+  alias PortfolioWeb, as: PW
   require Logger
 
   @supported_locales ["en", "ja"] # Define your supported locales
   @default_locale "en"
+  @static_paths PW.static_paths()
+
 
   def init(default), do: default
 
   def call(conn, _default) do
-    {locale_from_url, remaining_path} = extract_locale_from_path(conn.request_path)
+    if is_static_asset?(conn.request_path) do
+      conn
+    else
+      {locale_from_url, remaining_path} = extract_locale_from_path(conn.request_path)
 
-    # Handle root path ("/") and redirect to default locale if needed
-    if remaining_path == "" do
-      remaining_path = "/"
-    end
+      # # Handle root path ("/") and redirect to default locale if needed
+      # if remaining_path == "" do
+      #   remaining_path = "/"
+      # end
 
-    user_locale =
-      cond do
-        locale_from_url in @supported_locales -> locale_from_url
-        get_session(conn, "user_locale") -> get_session(conn, "user_locale")
-        get_preferred_language(conn) in @supported_locales -> get_preferred_language(conn)
-        true -> @default_locale
+      user_locale =
+        cond do
+          locale_from_url in @supported_locales -> locale_from_url
+          get_session(conn, "user_locale") -> get_session(conn, "user_locale")
+          get_preferred_language(conn) in @supported_locales -> get_preferred_language(conn)
+          true -> @default_locale
+        end
+
+      Logger.debug("Detected user_locale: #{user_locale}")
+
+      set_gettext_locale(user_locale)
+
+      # Redirect if the locale from the URL is unsupported or not present
+      if locale_from_url not in @supported_locales do
+        redirect_path = "/#{user_locale}#{remaining_path}"
+        conn = conn
+          |> put_session("user_locale", user_locale)
+          |> redirect(to: redirect_path)
+        halt(conn)
       end
 
-    Logger.debug("Detected user_locale: #{user_locale}")
-
-    set_gettext_locale(user_locale)
-
-    # Redirect if the locale from the URL is unsupported or not present
-    if locale_from_url not in @supported_locales do
-      redirect_path = "/#{user_locale}#{remaining_path}"
-      conn = conn
-        |> put_session("user_locale", user_locale)
-        |> redirect(to: redirect_path)
-      halt(conn)
+      conn
+      |> put_session("user_locale", user_locale)
     end
-
-    conn
-    |> put_session("user_locale", user_locale)
   end
+
+  defp is_static_asset?(path) do
+    @static_paths
+    |> Enum.any?(fn static_path ->
+        Regex.match?(~r/^\/#{static_path}(-\d*x\d*)?\.(png|jpg|jpeg|svg|ico)$/, path)
+    end)
+  end
+
 
   defp parse_accept_language_header(header) do
     header
