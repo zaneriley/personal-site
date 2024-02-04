693ebe3ba9cd6d6187bb03a119844aa9f21ec811
zaneriley <zaneriley@gmail.com>
2024-02-04 10:27:29 +0900

    Small refactor of case study controller

--- lib/portfolio_web/controllers/case_study_controller.ex
+++ lib/portfolio_web/controllers/case_study_controller.ex
@@ -14,31 +14,45 @@ defmodule PortfolioWeb.CaseStudyController do
         conn.assigns.user_locale
       )
 
+    {page_title, introduction} = set_page_metadata(case_study, translations)
+
     case case_study do
       nil ->
         Logger.error("Case study not found in database for URL: #{url}")
 
-      # Handle the error, e.g., render a 404 page or redirect
-      case_study ->
-        # Use translations to get the title and introduction
-        title = translations["title"] || case_study.title
-        introduction = translations["introduction"] || case_study.introduction
-
-        page_title =
-          "#{title} - " <>
-            gettext("Case Study") <>
-            " | " <>
-            gettext("Zane Riley | Product Design Portfolio")
-
-        Logger.info("Page title set to: #{page_title}")
-        Logger.debug("Page description set to: #{introduction}")
-
         conn
         |> assign(:case_study, case_study)
+        |> assign(:translations, translations)
+        |> assign(:page_title, "Not Found")
+        |> assign(:page_description, "The requested case study was not found.")
+        |> render("error.html")
+
+      %Portfolio.CaseStudy{} = cs ->
+        Logger.info("Case study found for URL: #{url}")
+
+        {page_title, introduction} = set_page_metadata(cs, translations)
+
+        conn
+        |> assign(:case_study, cs)
         |> assign(:translations, translations)
         |> assign(:page_title, page_title)
         |> assign(:page_description, introduction)
         |> render("show.html")
     end
+
+  end
+
+  # Moved out of the show/2 function
+  defp set_page_metadata(case_study, translations) do
+    title = translations["title"] || case_study.title
+    introduction = translations["introduction"] || case_study.introduction
+
+    page_title =
+      "#{title} - " <>
+        gettext("Case Study") <>
+        " | " <>
+        gettext("Zane Riley | Product Design Portfolio")
+
+    {page_title, introduction}
   end
 end
