b200a3225747e535443359245cccd66502563a7b
zaneriley <zaneriley@gmail.com>
2024-06-16 17:36:08 +0900

    skip a currently breaking test

--- test/portfolio_web/controllers/session_controller_test.exs
+++ test/portfolio_web/controllers/session_controller_test.exs
@@ -1,6 +1,12 @@
 defmodule PortfolioWeb.SessionControllerTest do
   use PortfolioWeb.ConnCase, async: true
 
+  # This is temporarily skipped because
+  # We disabled the session controller while
+  # we refactor the session logic into
+  # two separate channels, one for all sockets
+  # and the other for sockets like lang and theme.
+  @tag :skip
   describe "generate/2" do
     test "generates a token and user_id", %{conn: conn} do
       conn = post(conn, "/api/session/generate")
@@ -12,5 +18,5 @@ defmodule PortfolioWeb.SessionControllerTest do
       assert is_binary(response["token"])
       assert is_integer(response["user_id"])
     end
-  end
+  # end
 end
