f95d47f183aa6d9fa54a640a25b5d72668c857cf
zaneriley <zaneriley@gmail.com>
2024-06-26 13:32:59 +0900

    feat(ci): integrate Coveralls for test coverage reporting
    - Add GITHUB_TOKEN environment variable to GitHub Actions workflow
    - Update  function in  script to use
    - Ensure test coverage is reported to Coveralls

--- mix.exs
+++ mix.exs
@@ -12,7 +12,14 @@ defmodule Portfolio.MixProject do
       deps_path: "/mix/deps",
       aliases: aliases(),
       deps: deps(),
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test
+      ]
     ]
   end
 
