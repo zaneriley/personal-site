fc642b045310daa09adfbe9aac51461e76afc3b7
Zane Riley <zaneriley@gmail.com>
2024-07-04 11:00:51 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Portfolio.MixProject do
   def project do
     [
       app: :portfolio,
-      version: "0.1.0-alpha-1",
+      version: "0.1.0-alpha.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
