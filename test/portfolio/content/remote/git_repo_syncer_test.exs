4524da402169042d1dc0812fdcac48b95f132cda
zaneriley <zaneriley@gmail.com>
2024-09-14 22:19:47 +0900

    test(config): update tests for runtime configuration changes
    Modified tests to accommodate configuration changes and ensure they reflect the application's runtime environment.

--- test/portfolio/content/remote/git_repo_syncer_test.exs
+++ test/portfolio/content/remote/git_repo_syncer_test.exs
@@ -3,16 +3,7 @@ defmodule Portfolio.Content.FileManagement.GitRepoSyncerTest do
   alias Portfolio.Content.Remote.GitRepoSyncer
   require Logger
 
-  @valid_repo_url Application.compile_env(
-                    :portfolio,
-                    :content_repo_url
-                  )
   @invalid_repo_url "https://invalid-url.com/repo.git"
-  @local_path Application.compile_env(
-                :portfolio,
-                :content_base_path,
-                "priv/content"
-              )
 
   # The tests here are a bit brittle because of how they're expecting the folder to be cloned
 
@@ -28,84 +19,95 @@ defmodule Portfolio.Content.FileManagement.GitRepoSyncerTest do
   end
 
   setup do
+    valid_repo_url = Application.get_env(:portfolio, :content_repo_url)
+
+    local_path =
+      Application.get_env(:portfolio, :content_base_path, "priv/content")
+
     # Add these logging statements at the beginning of the setup block
     Logger.warn(
-      "Setup - valid_repo_url: #{inspect(@valid_repo_url)}, type: #{typeof(@valid_repo_url)}"
+      "Setup - valid_repo_url: #{inspect(valid_repo_url)}, type: #{typeof(valid_repo_url)}"
     )
 
     Logger.warn(
-      "Setup - local_path: #{inspect(@local_path)}, type: #{typeof(@local_path)}"
+      "Setup - local_path: #{inspect(local_path)}, type: #{typeof(local_path)}"
     )
 
-    unless File.exists?(@local_path) do
-      System.cmd("git", ["clone", @valid_repo_url, @local_path],
+    unless File.exists?(local_path) do
+      System.cmd("git", ["clone", valid_repo_url, local_path],
         env: %{"GIT_TERMINAL_PROMPT" => "0"},
         into: IO.stream(:stdio, :line)
       )
     end
 
-    :ok
+    {:ok, valid_repo_url: valid_repo_url, local_path: local_path}
   end
 
-  test "clones repository successfully" do
+  test "clones repository successfully", %{
+    valid_repo_url: valid_repo_url,
+    local_path: local_path
+  } do
     Logger.warn(
-      "Test clone - valid_repo_url: #{inspect(@valid_repo_url)}, type: #{typeof(@valid_repo_url)}"
+      "Test clone - valid_repo_url: #{inspect(valid_repo_url)}, type: #{typeof(valid_repo_url)}"
     )
 
     Logger.warn(
-      "Test clone - local_path: #{inspect(@local_path)}, type: #{typeof(@local_path)}"
+      "Test clone - local_path: #{inspect(local_path)}, type: #{typeof(local_path)}"
     )
 
-    assert {:ok, _repo} = GitRepoSyncer.sync_repo(@valid_repo_url, @local_path)
-    assert File.exists?(@local_path)
-    File.rm_rf!(@local_path)
+    assert {:ok, _repo} = GitRepoSyncer.sync_repo(valid_repo_url, local_path)
+    assert File.exists?(local_path)
+    File.rm_rf!(local_path)
   end
 
-  test "handles invalid repository URL" do
+  test "handles invalid repository URL", %{local_path: local_path} do
     # Add these logging statements at the beginning of the test
     Logger.warn(
       "Test invalid - invalid_repo_url: #{inspect(@invalid_repo_url)}, type: #{typeof(@invalid_repo_url)}"
     )
 
     Logger.warn(
-      "Test invalid - local_path: #{inspect(@local_path)}, type: #{typeof(@local_path)}"
+      "Test invalid - local_path: #{inspect(local_path)}, type: #{typeof(local_path)}"
     )
 
-    File.rm_rf!(@local_path)
+    File.rm_rf!(local_path)
 
     assert {:error, _reason} =
-             GitRepoSyncer.sync_repo(@invalid_repo_url, @local_path)
+             GitRepoSyncer.sync_repo(@invalid_repo_url, local_path)
 
-    refute File.exists?(@local_path)
+    refute File.exists?(local_path)
   end
 
-  test "handles existing directory" do
+  test "handles existing directory", %{
+    valid_repo_url: valid_repo_url,
+    local_path: local_path
+  } do
     # Add these logging statements at the beginning of the test
     Logger.warn(
-      "Test existing - valid_repo_url: #{inspect(@valid_repo_url)}, type: #{typeof(@valid_repo_url)}"
+      "Test existing - valid_repo_url: #{inspect(valid_repo_url)}, type: #{typeof(valid_repo_url)}"
     )
 
     Logger.warn(
-      "Test existing - local_path: #{inspect(@local_path)}, type: #{typeof(@local_path)}"
+      "Test existing - local_path: #{inspect(local_path)}, type: #{typeof(local_path)}"
     )
 
-    assert {:ok, _repo} = GitRepoSyncer.sync_repo(@valid_repo_url, @local_path)
-    assert File.exists?(@local_path)
+    assert {:ok, _repo} = GitRepoSyncer.sync_repo(valid_repo_url, local_path)
+    assert File.exists?(local_path)
   end
 
   # test "handles network issues" do
   #   # Simulate network issue by using an invalid URL
   #   assert {:error, _reason} =
-  #            GitRepoSyncer.sync_repo(@invalid_repo_url, @local_path)
+  #            GitRepoSyncer.sync_repo(@invalid_repo_url, local_path)
 
-  #   refute File.exists?(@local_path)
+  #   refute File.exists?(local_path)
   # end
 
   # test "clones to non-existent directory" do
   #   non_existent_path = "priv/non_existent_content"
 
   #   assert {:ok, _repo} =
-  #            GitRepoSyncer.sync_repo(@valid_repo_url, non_existent_path)
+  #            GitRepoSyncer.sync_repo(valid_repo_url, non_existent_path)
 
   #   assert File.exists?(non_existent_path)
   # end
@@ -116,8 +118,8 @@ defmodule Portfolio.Content.FileManagement.GitRepoSyncerTest do
   #   auth_options = [username: "username", password: "password"]
 
   #   assert {:ok, _repo} =
-  #            GitRepoSyncer.sync_repo(private_repo_url, @local_path, auth_options)
+  #            GitRepoSyncer.sync_repo(private_repo_url, local_path, auth_options)
 
-  #   assert File.exists?(@local_path)
+  #   assert File.exists?(local_path)
   # end
 end
